0b09392f6cda39b62fe74e9e4f26403f77f3f678
PasoStudio73 <paso.studio73@gmail.com>
2025-03-16 22:14:56 +0100

    marco

--- test/user_funcs/symbolic_analysis.jl
+++ test/user_funcs/symbolic_analysis.jl
@@ -12,20 +12,24 @@ X = X[chosen_rows, chosen_cols]
 y = y[chosen_rows]
 
 @testset "check every model" begin
-    model = symbolic_analysis(X, y; models=(type=:decisiontree,), preprocess=(;rng))
-    @test model.classifier == SoleXplorer.DecisionTreeClassifier
-    model = symbolic_analysis(X, y; models=(type=:randomforest,), preprocess=(;rng))
+    @test_nowarn model = symbolic_analysis(X, y; models=(type=:decisiontree,), preprocess=(;rng=Xoshiro(11)))
+    @test_nowarn model = symbolic_analysis(X, y; models=(type=:decisiontree_classifier,), preprocess=(;rng=Xoshiro(11)))
+    @test_nowarn model = symbolic_analysis(X, y; models=(type=:randomforest,), preprocess=(;rng=Xoshiro(11)))
+    @test_nowarn model = symbolic_analysis(X, y; models=(type=:randomforest_classifier,), preprocess=(;rng=Xoshiro(11)))
+    @test_nowarn model = symbolic_analysis(X, y; models=(type=:adaboost,), preprocess=(;rng=Xoshiro(11)))
+    @test_nowarn model = symbolic_analysis(X, y; models=(type=:adaboost_classifier,), preprocess=(;rng=Xoshiro(11)))
 
+    # TODO Marco, modalextractrules is not working with modal models, 
+    # I don't know if it's a SoleXplorer fault or PostHoc is still in developement with that
+    model = symbolic_analysis(X, y; models=(type=:modaldecisiontree,), preprocess=(;rng=Xoshiro(11)))
+    model = symbolic_analysis(X, y; models=(type=:modalrandomforest,), preprocess=(;rng=Xoshiro(11)))
+    model = symbolic_analysis(X, y; models=(type=:modaladaboost,), preprocess=(;rng=Xoshiro(11)))
+
+    # TODO Marco, I've made a small hack to let PostHoc working with XgBoost singular behavior:
+    # It's usual that XgBoost creates trees with no leafs.
+    # Please check PostHoc intrees.jl, line 184, branch devPaso
+    @test_nowarn model = symbolic_analysis(X, y; models=(type=:xgboost,), preprocess=(;rng=Xoshiro(11)))
+    @test_nowarn model = symbolic_analysis(X, y; models=(type=:xgboost_classifier,), preprocess=(;rng=Xoshiro(11)))
 end
 
-model = symbolic_analysis(X, y; models=(type=:decisiontree,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:decisiontree_classifier,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:randomforest,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:randomforest_classifier,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:adaboost,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:adaboost_classifier,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:modaldecisiontree,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:modalrandomforest,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:modaladaboost,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:xgboost,), preprocess=(;rng=Xoshiro(11)))
-model = symbolic_analysis(X, y; models=(type=:xgboost_classifier,), preprocess=(;rng=Xoshiro(11)))
+
