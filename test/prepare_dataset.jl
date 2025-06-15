f2fc6feb682815793540b02f1d0230b5e56aca27
PasoStudio73 <paso.studio73@gmail.com>
2025-06-15 12:59:05 +0200

    model type specification

--- test/prepare_dataset.jl
+++ test/prepare_dataset.jl
@@ -11,6 +11,7 @@ Xr = DataFrame(Xr)
 # ---------------------------------------------------------------------------- #
 #                        prepare dataset usage examples                        #
 # ---------------------------------------------------------------------------- #
+# basic setup
 modelc, dsc = prepare_dataset(Xc, yc)
 @test modelc isa SoleXplorer.Modelset
 @test dsc    isa SoleXplorer.Dataset
@@ -18,12 +19,69 @@ modelr, dsr = prepare_dataset(Xr, yr)
 @test modelr isa SoleXplorer.Modelset
 @test dsr    isa SoleXplorer.Dataset
 
-# dsc = prepare_dataset(
-#     Xc, yc;
-#     model=(;type=:decisiontree),
-#     preprocess=(;train_ratio=0.9, rng=Xoshiro(1)),
-#     measures=(log_loss, accuracy),
-# )
+# model type specification
+modelc, dsc = prepare_dataset(
+    Xc, yc;
+    model=(;type=:decisiontree)
+)
+@test modelc isa SoleXplorer.Modelset
+@test dsc    isa SoleXplorer.Dataset
+
+modelc, dsc = prepare_dataset(
+    Xc, yc;
+    model=(;type=:randomforest)
+)
+@test modelc isa SoleXplorer.Modelset
+@test dsc    isa SoleXplorer.Dataset
+
+modelc, dsc = prepare_dataset(
+    Xc, yc;
+    model=(;type=:adaboost)
+)
+@test modelc isa SoleXplorer.Modelset
+@test dsc    isa SoleXplorer.Dataset
+
+modelr, dsr = prepare_dataset(
+    Xr, yr;
+    model=(;type=:decisiontree)
+)
+@test modelr isa SoleXplorer.Modelset
+@test dsr    isa SoleXplorer.Dataset
+
+modelr, dsr = prepare_dataset(
+    Xr, yr;
+    model=(;type=:randomforest)
+)
+@test modelr isa SoleXplorer.Modelset
+@test dsr    isa SoleXplorer.Dataset
+
+modelc, dsc = prepare_dataset(
+    Xc, yc;
+    model=(;type=:modaldecisiontree)
+)
+@test modelc isa SoleXplorer.Modelset
+@test dsc    isa SoleXplorer.Dataset
+
+modelc, dsc = prepare_dataset(
+    Xc, yc;
+    model=(;type=:modalrandomforest)
+)
+@test modelc isa SoleXplorer.Modelset
+@test dsc    isa SoleXplorer.Dataset
+
+modelc, dsc = prepare_dataset(
+    Xc, yc;
+    model=(;type=:modaladaboost)
+)
+@test modelc isa SoleXplorer.Modelset
+@test dsc    isa SoleXplorer.Dataset
+
+modelc, dsc = prepare_dataset(
+    Xc, yc;
+    model=(;type=:xgboost)
+)
+@test modelc isa SoleXplorer.Modelset
+@test dsc    isa SoleXplorer.Dataset
 
 # dsc = prepare_dataset(
 #     Xc, yc;
