36e2287880a2ed5d2a7800f9ff78b210dfe85d7b
PasoStudio73 <paso.studio73@gmail.com>
2025-06-15 17:23:03 +0200

    tuning

--- test/train_test.jl
+++ test/train_test.jl
@@ -16,3 +16,70 @@ modelc = train_test(Xc, yc)
 @test modelc isa SoleXplorer.Modelset
 modelr = train_test(Xr, yr)
 @test modelr isa SoleXplorer.Modelset
+
+# ---------------------------------------------------------------------------- #
+#                                     tuning                                   #
+# ---------------------------------------------------------------------------- #
+# model type specification
+modelc = train_test(
+    Xc, yc;
+    model=(;type=:decisiontree),
+    tuning=(
+        method=(;type=grid),
+        ranges=(
+            SoleXplorer.range(:max_depth, lower=2, upper=10),
+            SoleXplorer.range(:feature_importance, values=[:impurity, :split])
+        )
+    ),
+    preprocess=(;rng=Xoshiro(1))
+)
+@test modelc isa SoleXplorer.Modelset
+
+modelc = train_test(
+    Xc, yc;
+    model=(;type=:randomforest),
+    tuning=(
+        method=(;type=randomsearch),
+        ranges=(
+            SoleXplorer.range(:max_depth, lower=2, upper=10),
+            SoleXplorer.range(:feature_importance, values=[:impurity, :split])
+        )
+    ),
+)
+@test modelc isa SoleXplorer.Modelset
+
+modelc = train_test(
+    Xc, yc;
+    model=(;type=:adaboost),
+    tuning=(
+        method=(;type=latinhypercube),
+        ranges=(SoleXplorer.range(:n_iter, lower=2, upper=10),)
+    ),
+)
+@test modelc isa SoleXplorer.Modelset
+
+modelr = train_test(
+    Xr, yr;
+    model=(;type=:decisiontree),
+        tuning=(
+        method=(;type=particleswarm),
+        ranges=(
+            SoleXplorer.range(:max_depth, lower=2, upper=10),
+            SoleXplorer.range(:feature_importance, values=[:impurity, :split])
+        )
+    ),
+)
+@test modelr isa SoleXplorer.Modelset
+
+modelr = train_test(
+    Xr, yr;
+    model=(;type=:randomforest),
+    tuning=(
+        method=(;type=adaptiveparticleswarm),
+        ranges=(
+            SoleXplorer.range(:max_depth, lower=2, upper=10),
+            SoleXplorer.range(:feature_importance, values=[:impurity, :split])
+        )
+    ),
+)
+@test modelr isa SoleXplorer.Modelset
