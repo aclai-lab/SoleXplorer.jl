c5abb0723ec345e1d236d7aee27bb322cc4eeb02
PasoStudio73 <paso.studio73@gmail.com>
2025-09-02 21:54:57 +0200

    Gio's fix #1

--- src/SoleXplorer.jl
+++ src/SoleXplorer.jl
@@ -46,7 +46,7 @@ modelc = symbolic_analysis(
     resample=CV(nfolds=5, shuffle=true),
     rng=Xoshiro(1),
     tuning=(tuning=Grid(resolution=10), resampling=CV(nfolds=3), range, measure=accuracy, repeats=2),
-    extractor=InTreesRuleExtractor(),
+    extractor=LumenExtractor(),
     measures=(accuracy, log_loss, confusion_matrix, kappa)      
 )
 ```
@@ -63,33 +63,37 @@ using  SoleModels: DecisionEnsemble, DecisionTree, DecisionXGBoost
 using  SoleModels: AbstractModel, solemodel, weighted_aggregation, apply!
 using  SoleModels: RuleExtractor, DecisionSet
 
-using  SolePostHoc
 @reexport using SolePostHoc: InTreesRuleExtractor, LumenRuleExtractor, BATreesRuleExtractor
 @reexport using SolePostHoc: RULECOSIPLUSRuleExtractor, REFNERuleExtractor, TREPANRuleExtractor
+using  SolePostHoc
 
-using ModalAssociationRules
 @reexport using ModalAssociationRules: Item, Atom, ScalarCondition, VariableMin, VariableMax
 @reexport using ModalAssociationRules: IA_L, box, diamond
 @reexport using ModalAssociationRules: gsupport, gconfidence, glift, gconviction, gleverage
+using ModalAssociationRules
 
 # ---------------------------------------------------------------------------- #
 #                                     MLJ                                      #
 # ---------------------------------------------------------------------------- #
-using  MLJ
-using  MLJ: MLJBase, MLJTuning
 
 # performance measures for classification
 @reexport using MLJ: accuracy, confusion_matrix, kappa, log_loss
 # performance measures for regression 
 @reexport using MLJ: rms, l1, l2, mae, mav
+# cross-validation
+@reexport using MLJ: Holdout, CV, StratifiedCV, TimeSeriesCV
+# tuning
+@reexport using MLJ: Grid, RandomSearch, LatinHypercube
+@reexport using MLJParticleSwarmOptimization: ParticleSwarm, AdaptiveParticleSwarm
+using  MLJ
+using  MLJ: MLJBase, MLJTuning
 
 # ---------------------------------------------------------------------------- #
 #                              external packages                               #
 # ---------------------------------------------------------------------------- #
+@reexport using SoleData: load_arff_dataset
 using  DataFrames
 using  Random
-
-@reexport using SoleData: load_arff_dataset
 
 # ---------------------------------------------------------------------------- #
 #                                   types                                      #
@@ -105,63 +109,59 @@ const Maybe{T} = Union{T, Nothing}
 #                                    utils                                     #
 # ---------------------------------------------------------------------------- #
 # feature extraction via Catch22
-using  Catch22
-include("featureset.jl")
-
 # export user friendly Catch22 nicknames
 export mode_5, mode_10, embedding_dist, acf_timescale, acf_first_min, ami2, trev, outlier_timing_pos
 export outlier_timing_neg, whiten_timescale, forecast_error, ami_timescale, high_fluctuation, stretch_decreasing
 export stretch_high, entropy_pairs, rs_range, dfa, low_freq_power, centroid_freq, transition_variance, periodicity
 export base_set, catch9, catch22_set, complete_set
+using  Catch22
+include("featureset.jl")
 
 # ---------------------------------------------------------------------------- #
 #                                 interfaces                                   #
 # ---------------------------------------------------------------------------- #
-@reexport using MLJ: Holdout, CV, StratifiedCV, TimeSeriesCV
-include("partition.jl")
+
 export partition
 export get_X, get_y, get_train, get_test
+include("partition.jl")
 
-include("treatment.jl")
 export WinFunction, MovingWindow, WholeWindow, SplitWindow, AdaptiveWindow
-
-@reexport using MLJ: Grid, RandomSearch, LatinHypercube
-@reexport using MLJParticleSwarmOptimization: ParticleSwarm, AdaptiveParticleSwarm
+include("treatment.jl")
 
 # ---------------------------------------------------------------------------- #
 #                                   models                                     #
 # ---------------------------------------------------------------------------- #
-using MLJDecisionTreeInterface
 @reexport using MLJDecisionTreeInterface: 
     DecisionTreeClassifier, DecisionTreeRegressor,
     RandomForestClassifier, RandomForestRegressor,
     AdaBoostStumpClassifier
+using MLJDecisionTreeInterface
 
-using ModalDecisionTrees
 @reexport using ModalDecisionTrees: 
     ModalDecisionTree, ModalRandomForest, ModalAdaBoost
+using ModalDecisionTrees
 
-using XGBoost, MLJXGBoostInterface
 @reexport using MLJXGBoostInterface: 
     XGBoostClassifier, XGBoostRegressor
+using XGBoost, MLJXGBoostInterface
 
 # ---------------------------------------------------------------------------- #
 #                                   modules                                    #
 # ---------------------------------------------------------------------------- #
-include("dataset.jl")
 export code_dataset, range
 export setup_dataset
+include("dataset.jl")
 
+export train_test
 include("apply.jl")
 include("train_test.jl")
-export train_test
 
+export Apriori, FPGrowth, Eclat
 include("extractrules.jl")
 include("associationrules.jl")
-export Apriori, FPGrowth, Eclat
 
-include("symbolic_analysis.jl")
 export symbolic_analysis, symbolic_analysis!
 export dsetup, solemodels, rules, associations
+include("symbolic_analysis.jl")
 
 end
