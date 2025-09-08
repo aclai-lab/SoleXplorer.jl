54c63dc0f5f5ae582d8ca7291727e5568b9b5c70
PasoStudio73 <paso.studio73@gmail.com>
2025-09-08 16:45:36 +0200

    minor

--- src/SoleXplorer.jl
+++ src/SoleXplorer.jl
@@ -73,7 +73,8 @@ export partition
 export get_X, get_y, get_train, get_test
 include("partition.jl")
 
-export WinFunction, MovingWindow, WholeWindow, SplitWindow, AdaptiveWindow
+export AbstractWinFunction, WinFunction
+export MovingWindow, WholeWindow, SplitWindow, AdaptiveWindow
 include("treatment.jl")
 
 # ---------------------------------------------------------------------------- #
