80fcbe41b1bec93e137c74cba7343a9101dfb153
PasoStudio73 <paso.studio73@gmail.com>
2025-09-14 18:05:28 +0200

    first build

--- docs/make.jl
+++ docs/make.jl
@@ -3,23 +3,24 @@ using SoleXplorer
 
 # DocMeta.setdocmeta!(SoleXplorer, :DocTestSetup, :(using SoleXplorer); recursive = true)
 
-# makedocs(;
-#     modules=[SoleXplorer],
-#     authors=["Lorenzo BALBONI", "Mauro MILELLA", "Giovanni PAGLIARINI", "Alberto PAPARELLA", "Riccardo PASINI", "Marco PERROTTA"],
-#     repo=Documenter.Remotes.GitHub("aclai-lab", "SoleXplorer.jl"),
-#     sitename="SoleXplorer.jl",
-#     format=Documenter.HTML(;
-#         size_threshold=4000000,
-#         prettyurls=get(ENV, "CI", "false") == "true",
-#         canonical="https://aclai-lab.github.io/SoleXplorer.jl",
-#         assets=String[],
-#     ),
-#     pages=[
-#         "Home"      => "index.md",
-#         "Reference" => "reference.md",
-#     ],
-#     warnonly=:true,
-# )
+makedocs(;
+    modules=[SoleXplorer],
+    authors="Lorenzo Balboni, Mauro Milella, Giovanni Pagliarini, Alberto Paparella, Riccardo Pasini, Marco Perrotta",
+    repo=Documenter.Remotes.GitHub("aclai-lab", "SoleXplorer.jl"),
+    sitename="SoleXplorer.jl",
+    format=Documenter.HTML(;
+        size_threshold=4000000,
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://aclai-lab.github.io/SoleXplorer.jl",
+        edit_link="main", # possibly this line is dangerous after publishing
+        assets=String[],
+    ),
+    pages=[
+        "Home"      => "index.md",
+        # "Reference" => "reference.md",
+    ],
+    warnonly=:true,
+)
 
 # deploydocs(;
 #     repo = "github.com/aclai-lab/SoleXplorer.jl",
