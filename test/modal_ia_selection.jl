a3c8d32fa50ccea07f01966ed7ac55afb2d191b2
PasoStudio73 <paso.studio73@gmail.com>
2025-11-27 13:05:01 +0100

    ia relations bug

---
+++ test/modal_ia_selection.jl
@@ -0,0 +1,101 @@
+using Test
+using SoleXplorer
+const SX = SoleXplorer
+
+# using SoleData
+using MLJ
+using DataFrames, Random
+
+@show Threads.nthreads()
+
+# ---------------------------------------------------------------------------- #
+#                                load dataset                                  #
+# ---------------------------------------------------------------------------- #
+natopsloader = NatopsLoader()
+Xts, yts = SX.load(natopsloader)
+
+# ---------------------------------------------------------------------------- #
+#                      realtions in modal decision trees                       #
+# ---------------------------------------------------------------------------- #
+dsts_ia = symbolic_analysis(
+    Xts, yts;
+    model=ModalDecisionTree(; relations=:IA),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
+
+dsts_ia3 = symbolic_analysis(
+    Xts, yts;
+    model=ModalDecisionTree(; relations=:IA3),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
+
+dsts_ia7 = symbolic_analysis(
+    Xts, yts;
+    model=ModalDecisionTree(; relations=:IA7),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
+
+dsts_rcc5 = symbolic_analysis(
+    Xts, yts;
+    model=ModalDecisionTree(; relations=:RCC5),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
+
+dsts_rcc8 = symbolic_analysis(
+    Xts, yts;
+    model=ModalDecisionTree(; relations=:RCC8),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
+
+# ---------------------------------------------------------------------------- #
+#                       realtions in modal random forest                       #
+# ---------------------------------------------------------------------------- #
+dsts_ia = symbolic_analysis(
+    Xts, yts;
+    model=ModalRandomForest(; relations=:IA),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
+
+dsts_ia3 = symbolic_analysis(
+    Xts, yts;
+    model=ModalRandomForest(; relations=:IA3),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
+
+dsts_ia7 = symbolic_analysis(
+    Xts, yts;
+    model=ModalRandomForest(; relations=:IA7),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
+
+dsts_rcc5 = symbolic_analysis(
+    Xts, yts;
+    model=ModalRandomForest(; relations=:RCC5),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
+
+dsts_rcc8 = symbolic_analysis(
+    Xts, yts;
+    model=ModalRandomForest(; relations=:RCC8),
+    resampling=Holdout(fraction_train=0.7, shuffle=true),
+    seed=1,
+    win=AdaptiveWindow(nwindows=3, relative_overlap=0.3),
+)
